eb811c189eace2c19708c1704f378567874d6eee
rui dinis <ruidinis@sapo.pt>
2021-02-26 16:03:21 +0000

    05

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate / 100;
+    
     
 }
         
