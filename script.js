32adaaa3d616d9a1b5ea3e46c07f09376d32a864
rui dinis <ruidinis@sapo.pt>
2021-03-02 16:41:15 +0000

    all

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate / 100;
-    var currentYear = now.getFullYear()
-    console.log("Current year is: " + currentYear);
+var principal = document.getElementById("principal");
+var rate = 	document.getElementById("rate");
+var years = document.getElementById("years");
+var currentYear = new Date().getFullYear();
 
-    
+console.log(principal,rate,years)
+
+function interest_value(){
+  var x = document.getElementById("rate").value;
+  document.getElementById("current_rate").innerHTML = x + " %";
 }
-   compute()     
+
+function compute(){
+	var deposit = (principal.value);
+	var rate_final = (rate.value);
+	var future_years = currentYear + (years.value)
+	var amount = deposit * future_years * rate_final / 100
+	
+	document.getElementById("deposit").innerHTML = deposit;
+	document.getElementById("rate_final").innerHTML = rate_final;
+	document.getElementById("future_years").innerHTML = future_years;
+	document.getElementById("amount").innerHTML = amount;
+	document.getElementById("result").style.display = "block";
+}
+  
